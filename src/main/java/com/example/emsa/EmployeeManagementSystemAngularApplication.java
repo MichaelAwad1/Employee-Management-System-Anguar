f78b90304a4b3aa873fd9c72ada97a736a081d12
Michael Awad <michael@frona.net>
2022-01-01 13:13:25 +0200

    starter project and configure application.properties

---
+++ src/main/java/com/example/emsa/EmployeeManagementSystemAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.emsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementSystemAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emsa/EmployeeManagementSystemAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
